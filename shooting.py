63ba88bad8994c1bdc1f038c5b5ceffa2794ee19
mkmk2 <moke2jp@gmail.com>
2020-11-29 22:56:07 +0900

    敵セット処理見直し

--- shooting.py
+++ shooting.py
@@ -9,7 +9,7 @@ import plitem
 
 # --------------------------------------------------
 # 敵のセット位置
-STAGE_EM_POS = [            # 時間, X, Y, id0, id1, item
+STAGE_EM_POS = [            # 時間, X, Y, class, id0, id1, item
 
 #    [ 100,  128-40,   0,  enemy.EnemyWide,  0, 1,],    # 撃ってもどる
 #    [ 150,  128-40,   0,  enemy.EnemyWide,  0, 1,],    # 撃ってもどる
@@ -30,45 +30,42 @@ STAGE_EM_POS = [            # 時間, X, Y, id0, id1, item
 #STAGE_EM_POS = [            # 時間, X, Y, id0, id1, item
 
 
-#    [ 100, 128,   0, EMBOSS,  0, 0,],   # Boss
+    [ 150,  128-40,   0,  enemy.EnemyWide,  0, 0, 1,],    # 撃ってもどる
 
-    [ 150,  128-40,   0,  enemy.EnemyWide,  0, 1,],    # 撃ってもどる
+#    [ 200,  128,      0,  enemy.EnemyBoss,  0, 0, 0,],    # Boss
 
-    [ 200,  128,      0,  enemy.EnemyBoss,  0, 0,],    # Boss
+    [ 200,  128-60,   0,  enemy.EnemyWide,  0, 0, 0,],    # まっすぐ
+    [ 240,  128-60,   0,  enemy.EnemyWide,  0, 0, 0,],
+    [ 280,  128-60,   0,  enemy.EnemyWide,  0, 0, 1,],
 
-    [ 200,  128-60,   0,  enemy.EnemyWide,  0, 0,],    # まっすぐ
-    [ 240,  128-60,   0,  enemy.EnemyWide,  0, 0,],
-    [ 280,  128-60,   0,  enemy.EnemyWide,  0, 1,],
+    [ 500,  128+60,   0,  enemy.EnemyWide,  0, 0, 0,],
+    [ 540,  128+60,   0,  enemy.EnemyWide,  0, 0, 0,],
+    [ 580,  128+60,   0,  enemy.EnemyWide,  0, 0, 1,],
 
-    [ 500,  128+60,   0,  enemy.EnemyWide,  0, 0,],
-    [ 540,  128+60,   0,  enemy.EnemyWide,  0, 0,],
-    [ 580,  128+60,   0,  enemy.EnemyWide,  0, 1,],
+    [ 760,  128-20,   0,  enemy.EnemyNorm,  0, 0, 0,],    # カーブ
+    [ 780,  128-40,   0,  enemy.EnemyNorm,  0, 0, 0,],
+    [ 800,  128-60,   0,  enemy.EnemyNorm,  0, 0, 1,],
 
-    [ 760,  128-20,   0,  enemy.EnemyNorm,  0, 0,],    # カーブ
-    [ 780,  128-40,   0,  enemy.EnemyNorm,  0, 0,],
-    [ 800,  128-60,   0,  enemy.EnemyNorm,  0, 1,],
+    [1000,  128+20,   0,  enemy.EnemyNorm,  0, 0, 0,],
+    [1020,  128+40,   0,  enemy.EnemyNorm,  0, 0, 0,],
+    [1040,  128+60,   0,  enemy.EnemyNorm,  0, 0, 1,],
 
-    [1000,  128+20,   0,  enemy.EnemyNorm,  0, 0,],
-    [1020,  128+40,   0,  enemy.EnemyNorm,  0, 0,],
-    [1040,  128+60,   0,  enemy.EnemyNorm,  0, 1,],
+    [1300,  128-40,   0,  enemy.EnemyNorm,  0, 0, 0,],    # カーブ
+    [1340,  128-60,   0,  enemy.EnemyNorm,  0, 0, 0,],
+    [1380,  128-80,   0,  enemy.EnemyNorm,  0, 0, 1,],
 
-    [1300,  128-40,   0,  enemy.EnemyNorm,  0, 0,],    # カーブ
-    [1340,  128-60,   0,  enemy.EnemyNorm,  0, 0,],
-    [1380,  128-80,   0,  enemy.EnemyNorm,  0, 1,],
+    [1600,  128+40,   0,  enemy.EnemyNorm,  0, 0, 1,],
+    [1640,  128+60,   0,  enemy.EnemyNorm,  0, 0, 1,],
+    [1680,  128+80,   0,  enemy.EnemyNorm,  0, 0, 1,],
 
-    [1600,  128+40,   0,  enemy.EnemyNorm,  0, 1,],
-    [1640,  128+60,   0,  enemy.EnemyNorm,  0, 1,],
-    [1680,  128+80,   0,  enemy.EnemyNorm,  0, 1,],
+    [1750,  128-40,   0,  enemy.EnemyWide,  0, 0, 1,],    # 撃ってもどる
+    [1800,  128+40,   0,  enemy.EnemyWide,  0, 0, 1,],
 
-    [1750,  128-40,   0,  enemy.EnemyWide,  0, 1,],    # 撃ってもどる
-    [1800,  128+40,   0,  enemy.EnemyWide,  0, 1,],
+    [1900,  128-00,   0,  enemy.EnemyWide,  2, 0, 1,],    # 全方向撃って戻る
+    [2300,  128-40,   0,  enemy.EnemyWide,  2, 0, 1,],    # 全方向撃って戻る
+    [2700,  128+40,   0,  enemy.EnemyWide,  2, 0, 1,],    # 全方向撃って戻る
 
-    [1900,  128-00,   0,  enemy.EnemyWide,  2, 1,],    # 全方向撃って戻る
-    [2300,  128-40,   0,  enemy.EnemyWide,  2, 1,],    # 全方向撃って戻る
-    [2700,  128+40,   0,  enemy.EnemyWide,  2, 1,],    # 全方向撃って戻る
-
-    [3500,  128,      0,  enemy.EnemyBoss,  0, 0,],    # Boss
-
+    [3500,  128,      0,  enemy.EnemyBoss,  0, 0, 0,],    # Boss
 ]
 
 # ==================================================
@@ -364,11 +361,23 @@ class SceneGameMain:
     #  ------------------------------------------
     def SetStageEnemy(self):
         # ステージの位置から敵をセットする
-        for n,e in enumerate(STAGE_EM_POS):
-            if self.Stage_Pos == e[0]:
+        l = len(STAGE_EM_POS)
+        n = 0
 
+        while l > 0:
+            e = STAGE_EM_POS[n]
+            if self.Stage_Pos == e[0]:
                 t = e[3]
-                imp.Em.append(t(e[1], e[2], e[3], e[4], e[5]))
+                imp.Em.append(t(e[1], e[2], e[4], e[5], e[6]))
+                break
+            l -= 1
+            n += 1
+
+#        for n,e in enumerate(STAGE_EM_POS):
+#            if self.Stage_Pos == e[0]:
+
+#                t = e[3]
+#                imp.Em.append(t(e[1], e[2], e[3], e[4], e[5]))
 
     #  ------------------------------------------
     def CheckColli(self, plat, embd):       # plat 攻撃側　　embd ダメージ側
